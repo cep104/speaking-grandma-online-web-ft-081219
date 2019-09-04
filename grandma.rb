03a00d1660736c3c503e6ee662c950d3da439fa5
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-04 01:09:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,13 @@
+def speak_to_grandma(talk)
+  if talk == "I LOVE YOU GRANDMA!" 
+     "I LOVE YOU TOO PUMPKIN!"
+  elsif talk == talk.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
