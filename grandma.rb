7a62216f75b61b8c1c436ab8457151ac229e18b6
cep104 <candicelizabethpeters@gmail.com>
2020-10-27 21:23:06 -0500

    commit tree 28f49fd7946b8531605609b39a13c32d4fa66f67

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,20 @@
+require 'pry'
 # Write a speak_to_grandma method.
+def speak_to_grandma(speak)
+    
+    if speak != speak.upcase
+        puts "HUH?! SPEAK UP, SONNY!"
+        return  "HUH?! SPEAK UP, SONNY!"
+    elsif speak == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    else 
+        binding.pry
+        puts "NO, NOT SINCE 1938!"
+        return "NO, NOT SINCE 1938!"
+    end
+end
 
+speak_to_grandma("HELLO")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -11,3 +26,4 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
